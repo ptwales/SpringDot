07f059afd9f5ea56f2260bc7b7873c5d4808b957
ptwales <ptwales@gmail.com>
2013-09-11 20:18:44 -0500

    Extended testing in 2dVec and started writing sheet by time and sheet by el

--- answer/storage.h
+++ answer/storage.h
@@ -5,19 +5,54 @@
 
 namespace storage
 {
-	class sheetByTime	:public twoDVec
+
+	class abstractSheet	:public twDVec
 	{
 	public:
-		typedef std::vector<bool> boolVec;
-		
-		sheetByTime ( unsigned int width, unsigned int height, user_prec init_fill
-		void pack ( y_vec )
+		typedef std::vector<bool> boolVec_t;
+		virtual void pack ( y_vec ) = 0;
+		void clear ( ) { last = sheet.begin(); }
 
+	protected:
+		boolVec_t orders;
+		sheet_t::iterator last;
 	};
 
+	/*
+	 * sheetByTime:
+	 * sub vectors are y(t)
+	 */
+	class sheetByTime	:public abstractSheet 
+	{
+	public:
+		sheetByTime ( unsigned int length, boolVec_t _orders, user_prec init_fill=filler) : twoDVec ( length, _orders.size(), filler )
+		{
+			orders = _orders;
+			last = sheet.begin();
+		}
+
+		sheetByTime ( const sheetByTime& original, boolVec _orders ) : twoDVec ( original )
+		{
+			// twoDVec copy constructer has copied the sheet
+			// now remove undesired elements
+			// This can be a pain with this method
+		}
+
+		void pack ( y_vec )
+		{
+			return;
+		}
+
+	};
+	/*
+	 * sheetByEl:
+	 * sub vectors are  
+	 */
+/*
 	class sheetByEl		:public twoDVec
 	{
 	};
+*/
 
 }
 #endif // STORAGE_H
--- answer/twoDVec.h
+++ answer/twoDVec.h
@@ -8,25 +8,22 @@ namespace storage
 	class twoDVec
 	{
 	public:
-		typedef std::vector<user_prec> row_t;
-		typedef std::vector<row_t> sheet_t;
+		typedef std::vector<y_vec> sheet_t;
 
-		twoDVec ( unsigned int width=1, unsigned int height=1, user_prec init_fill=filler) 
+		twoDVec ( unsigned int length=1, unsigned int height=1, user_prec init_fill=filler) 
 		{
-			sheet.resize( width, row_t( height, init_fill) );
+			sheet.resize( length, y_vec( length, init_fill) );
 		}
 
-		twoDVec ( unsigned int width, row_t init_vec ) 
+		twoDVec ( unsigned int length, y_vec init_vec ) 
 		{
-			sheet.resize( width, init_vec );
+			sheet.resize( length, init_vec );
 		}
 
 		twoDVec ( const twoDVec& original )
 		{
 			sheet = original.sheet;
 		}
-
-		//virtual void pack ( row_t ) = 0;
 
 		sheet_t sheet;
 	};
